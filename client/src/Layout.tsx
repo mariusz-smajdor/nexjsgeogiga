2421417e4316beb514f3e51673573ce307c3b2a0
Mariusz Smajdor <smajdormariusz98@gmail.com>
2026-04-19 16:53:13 +0200

    change file naming convention

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 
-import { Header } from './components/layout/header/Header';
+import { Header } from './components/layout/header';
 
 function Layout() {
   return (
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import Layout from './Layout.tsx';
-import HomePage from './pages/home/Home.tsx';
+import Layout from './layout.tsx';
+import HomePage from './pages/home/index.tsx';
 
 export const router = createBrowserRouter([
   {
